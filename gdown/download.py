30d8cbcd2996c5639a3a0cbb157dbc66b940ada6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-17 17:28:04 +0530

    Update download.py

--- gdown/download.py
+++ gdown/download.py
@@ -237,7 +237,7 @@ def download(
         if total is not None:
             total = int(total)
         if not quiet:
-            pbar = tqdm.tqdm(desc=f"GDRIVE FILE DOWN for {sender_id}\n\n", total=total, unit="B", unit_scale=True)
+            pbar = tqdm.tqdm(desc=f"GDRIVE FILE DOWN for {sender_id}", total=total, unit="B", unit_scale=True)
         t_start = time.time()
         for chunk in res.iter_content(chunk_size=CHUNK_SIZE):
             f.write(chunk)
